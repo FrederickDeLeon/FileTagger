1cd8e732af3557583d6fbdf684bea7ff985c2b46
Frederick De Leon <freddy.deleon16@gmail.com>
2025-10-23 22:37:29 -0400

    Reapply "Initial project setup"
    This reverts commit 6a735d13f50c75d7480f66982dc8208efbf8466a.

---
+++ FileTagger/FileTagger.cpp
@@ -0,0 +1,26 @@
+﻿// FileTagger.cpp : Defines the entry point for the application.
+//
+
+#include "FileTagger.h"
+#include "TagManager.h"
+
+
+using namespace std;
+
+#include <iostream>
+
+int main() {
+    TagManager manager;
+
+    manager.addTags("C:/example/file1.txt", { "work", "cpp", "important" });
+    manager.addTags("C:/example/file1.txt", { "urgent", "cpp" }); // make sure duplicates can be dealt with as well 
+
+    auto tags = manager.getTags("C:/example/file1.txt");
+
+    std::cout << "Tags for file1:" << std::endl;
+    for (const auto& tag : tags) {
+        std::cout << "- " << tag << std::endl;
+    }
+
+    return 0;
+}
